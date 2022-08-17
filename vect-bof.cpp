8ef1315d223ccbfb82c5476435e36bf0ad165c90
csloley1 <74067123+csloley1@users.noreply.github.com>
2022-08-17 09:06:12 -0400

    Create vect-bof.cpp

---
+++ vect-bof.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+
+using std::cout;
+using std::endl;
+using std::vector;
+
+int main(void) {
+    vector<int> vect;
+    cout << vect[5] << endl;
+    return 0;
+}
